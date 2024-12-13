35a4c8d66fc7d882172ec88153c107b828ba4c33
Ben <ben.lau@acb.studio>
2024-12-14 00:30:47 +0100

    js condition checks

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,50 @@
 const password = document.getElementById("password");
 const conditions = [...document.getElementById("conditions").children];
+const conditionSpecialChar = document.getElementById("condition-special-char");
+const conditionUppercase = document.getElementById("condition-upper-char");
+const conditionLowercase = document.getElementById("condition-lower-char");
 
-conditions.forEach((condition) => {
-  if (condition.dataset.condition === "❌") {
-    condition.innerHTML = `❌ ${condition.innerHTML}`;
-  }
-});
+function updateConditions() {
+  conditions.forEach((condition) => {
+    const status = condition.dataset.valid === "true";
+    condition.querySelector("span").innerHTML = status ? "✅ " : "❌ ";
+    condition.classList.toggle("text-green-500", status);
+  });
+}
 
-function validateConditions(pw) {
-  console.log(pw);
+function checkMinLength(pw) {
+  document.getElementById("condition-min-num").dataset.valid =
+    pw.length >= 8 ? true : false;
+}
+
+function checkHasNum(pw) {
+  document.getElementById("condition-has-num").dataset.valid = pw.match(/\d+/g)
+    ? true
+    : false;
+}
+
+function checkSpecialChar(pw) {
+  document.getElementById("condition-special-char").dataset.valid = pw.match(
+    /[^A-Za-z0-9]/g
+  )
+    ? true
+    : false;
+}
+
+function checkUppercase(pw) {
+  document.getElementById("condition-upper-char").dataset.valid = pw.match(
+    /[A-Z]/g
+  )
+    ? true
+    : false;
+}
+
+function checkLowercase(pw) {
+  document.getElementById("condition-lower-char").dataset.valid = pw.match(
+    /[a-z]/g
+  )
+    ? true
+    : false;
 }
 
 password.addEventListener("input", () => {
@@ -16,5 +52,12 @@ password.addEventListener("input", () => {
   const pw = password.value;
 
   // 2. For every keystroke, check against list of conditions
-  validateConditions(pw);
+  checkMinLength(pw);
+  checkHasNum(pw);
+  checkSpecialChar(pw);
+  checkUppercase(pw);
+  checkLowercase(pw);
+  updateConditions();
 });
+
+updateConditions();
